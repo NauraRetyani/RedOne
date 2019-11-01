b6398e0c9854e133cc3cfaca551d65cde6f0acbf
naura <naura.retyani@gmail.com>
2019-11-01 22:00:30 +0700

    layout

--- app/src/main/java/com/theraza/idn/redone/MainActivity.java
+++ app/src/main/java/com/theraza/idn/redone/MainActivity.java
@@ -33,11 +33,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-
-
-
-
-
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
