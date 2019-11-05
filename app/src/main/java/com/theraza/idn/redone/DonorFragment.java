219eab486965e1bd733403e46e28b9e47ba3d90f
naura <naura.retyani@gmail.com>
2019-11-05 14:56:45 +0700

    add Cari Donor

---
+++ app/src/main/java/com/theraza/idn/redone/DonorFragment.java
@@ -0,0 +1,31 @@
+package com.theraza.idn.redone;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DonorFragment extends Fragment {
+
+
+    public DonorFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_donor, container, false);
+    }
+
+}
--- app/src/main/java/com/theraza/idn/redone/Fragment/SearchFragment.java
+++ app/src/main/java/com/theraza/idn/redone/Fragment/SearchFragment.java
@@ -3,20 +3,30 @@ package com.theraza.idn.redone.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.theraza.idn.redone.DonorFragment;
 import com.theraza.idn.redone.R;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements View.OnClickListener {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnCategory = view.findViewById(R.id.btn_cari_donor);
+        btnCategory.setOnClickListener(this);
+    }
 
     public SearchFragment() {
         // Required empty public constructor
@@ -31,4 +41,18 @@ public class SearchFragment extends Fragment {
 
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_cari_donor) {
+            DonorFragment donorFragment = new DonorFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            if (fragmentManager != null) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, donorFragment, DonorFragment.class.getSimpleName())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        }
+
+    }
 }
